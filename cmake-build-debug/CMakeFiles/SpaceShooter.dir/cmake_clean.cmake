eb1d0aead82c7aa618271ed0216f60f306a74541
Valanche <block_g@163.com>
2022-01-22 14:26:20 +0800

    samples

---
+++ cmake-build-debug/CMakeFiles/SpaceShooter.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SpaceShooter.dir/SpaceShooter.c.obj"
+  "SpaceShooter.exe"
+  "SpaceShooter.exe.manifest"
+  "SpaceShooter.pdb"
+  "libSpaceShooter.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SpaceShooter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
